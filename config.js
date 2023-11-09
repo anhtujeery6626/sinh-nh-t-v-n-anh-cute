8ec950f32130a48ffe516c3190fc9061f6b59c8c
anhtujeery6626 <125799039+anhtujeery6626@users.noreply.github.com>
2023-11-09 16:01:19 +0700

    va cute
    chúc mừng sinh nhật vân anh

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Vân Anh nhé';
+const giftUrl = 'file:///C:/Users/hacker/Downloads/thiep-mung-main%20(1)/thiep-mung-main/hpbd.html';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Gửi Vân Anh ';
+const contentCard = 'Chúc cậu có một ngày sinh nhật tuyệt vời bên gia đình, bạn bè và đặc biệt là bên tớ. Hy vọng cuộc sống của cậu sẽ mãi tràn ngập tiếng cười và hạnh phúc';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'va1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
